e1c5aa84a53f043a4fbcc1b7f3525f8ab1f6b248
MegaGlub <45982508+MegaGlub@users.noreply.github.com>
2024-02-01 18:32:08 -0700

    Added a preliminary test page

--- test-app/main.js
+++ test-app/main.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('test.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
