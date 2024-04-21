c96f0fcb6b3d0038d146bb871ff8cdc29167ab80
MegaGlub <157172346+C-Szott@users.noreply.github.com>
2024-04-20 19:57:08 -0600

    autodetect json files + json to path function

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
     width: 1920, 
     height: 1080, //these guys have real computers
     titleBarStyle: 'default'
