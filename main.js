667a15f4f164acdab2a0f9ae7c7d37ac38b3e8ea
MegaGlub <157172346+C-Szott@users.noreply.github.com>
2024-05-31 20:11:50 -0600

    Purpose components exist now

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 var fs = module.require('fs');
 
+import { spellComponent } from "./src/spellComponent.js";
+import { formComponent } from "./src/formComponent.js";
+import { pathComponent } from "./src/pathComponent.js";
+import { triggerComponent } from "./src/triggerComponent.js";
+import { enhancementComponent } from "./src/enhancementComponent.js";
+import { readJSONDirectory } from "./src/json.js";
+
 const mainMenuScreen = document.getElementById("mainMenuScreen");
 const spellBox = document.getElementById("spellBox");
 var componentList = []; //Components should use componentList.push(constructor); when being made
+
+//finish purposeComponent.js
+//sort the componentList by some attribute (add configuration for it?)
+//find somewhere to call spellComponent's drawElement()
+//work on the wand box
+//work on the martial arts
 
 const mainMenuSpellsButton = document.getElementById("mainMenuSpellsButton");
 const mainMenuMartialsButton = document.getElementById("mainMenuMartialsButton");
@@ -13,13 +26,6 @@ assignClickableButtonByID("mainMenuButton", unhideMainMenu);
 
 
 readJSONDirectory("data/components");
-
-import { spellComponent } from "./src/spellComponent.js";
-import { formComponent } from "./src/formComponent.js";
-import { pathComponent } from "./src/pathComponent.js";
-import { triggerComponent } from "./src/triggerComponent.js";
-import { enhancementComponent } from "./src/enhancementComponent.js";
-import { readJSONDirectory } from "./src/json.js";
 
 function assignClickableButtonByID(elementId, funct){
     const element = document.getElementById(elementId);
---
+++ src/purposeComponent.js
@@ -0,0 +1,28 @@
+import { spellComponent } from "./spellComponent.js";
+export class enhancementComponent extends spellComponent { //This feels incomplete, but I'm pretty sure it's done.
+    constructor(name, description, purposeDescription, image, primaryCost, primaryType, secondaryCost, secondaryType, targetType){
+        super(name, "Purpose", description, image, primaryCost, primaryType, secondaryCost, secondaryType, energyCost, 0);
+        this.purposeDescription = purposeDescription;
+        this.targetType = targetType;
+        this.buildEnhancementVisuals();
+    }
+
+    buildEnhancementVisuals() {
+        this.#createEmptyElements();
+        this.#assignElementClasses();
+        this.#relateElements();
+        this.#fillInnerHTML();
+      }
+      #createEmptyElements() {
+        this.targetCellElement = document.createElement("td");
+      }
+      #assignElementClasses() {
+        this.targetCellElement.className = "componentStatCell";
+      }
+      #relateElements() {
+        this.modifierRowElement.appendChild(this.targetCellElement);
+      }
+      #fillInnerHTML() {
+        this.targetCellElement.innerHTML = "Targets: " + this.targetType;
+      }
+}
