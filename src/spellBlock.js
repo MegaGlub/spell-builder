8c85530b98d05b1e1671c22435ccdc2487d7c2a4
MegaGlub <45982508+MegaGlub@users.noreply.github.com>
2024-09-03 18:02:24 -0600

    split spellBlocks from wands

---
+++ src/spellBlock.js
@@ -0,0 +1,242 @@
+import { logText } from "./logging.js";
+import { clearChildren } from "./elementHelpers.js";
+
+export class spellBlock {
+    constructor(spellsByComponent, positionInWand, descriptionElement) {
+        this.spells = spellsByComponent;
+        this.positionInWand = positionInWand;
+        this.errorBox = "";
+        this.descriptionElement = descriptionElement;
+
+        this.textBox = this.descriptionElement.children[0];
+        this.statBox = this.descriptionElement.children[1];
+        this.errorBox = this.descriptionElement.children[2];
+
+        this.voidComponent = this.#findComponentByType("Void");
+        this.pathComponent = this.#findComponentByType("Path");
+        this.enhancementComponents = this.#findAllComponentsByType("Enhancement");
+        this.formComponent = this.#findComponentByType("Form");
+        this.purposeComponents = this.#findAllComponentsByType("Purpose");
+        this.triggerComponent = this.#findComponentByType("Trigger");
+
+        this.#discoverBasicStats();
+    }
+
+    #discoverBasicStats(){ //more stats to be add: dmg, dice type, hit type, hit mod, etc.
+        this.inverted = false;
+        if (this.voidComponent) {
+            this.inverted = true;
+        }
+        this.potency = 0;
+        for (let component of this.spells) {
+            this.potency += component.potencyModifier;
+        }
+    }
+
+    compileSpell(){
+        if (this.#errorTest()) {
+            this.#addDescriptionText("Spell has one or more fatal errors and cannot be compiled!");
+        } else {
+            this.#fillPathText();
+            this.#fillFormText();
+            this.#fillEnhancementText();
+            this.#fillPurposeText();
+            this.#fillTriggerText();
+        }
+    }
+
+    #fillPathText(){
+        this.#addDescriptionText(this.pathComponent.pathDescription, this.pathComponent.type);
+    }
+
+    #fillFormText(){
+        this.#addDescriptionText(this.formComponent.formDescription, this.formComponent.type);
+    }
+
+    #fillEnhancementText(){
+        if (this.enhancementComponents.length > 0) {
+            for (let enhancement of this.enhancementComponents) {
+                this.#addDescriptionText(enhancement.enhancementDescription, enhancement.type);
+            }
+        }
+    }
+
+    #fillPurposeText() {
+        for (let i = 0; i < this.purposeComponents.length - 1; i++){
+            this.#addPurposeToText(this.purposeComponents[i]);
+            this.#addDescriptionText(" and ");
+        }
+        this.#addPurposeToText(this.purposeComponents[this.purposeComponents.length - 1]);
+        this.#addDescriptionText(". ");
+    }
+
+    #fillTriggerText(){
+        if (this.triggerComponent) {
+            this.#addDescriptionText(this.triggerComponent.triggerDescription, this.triggerComponent.type);
+        }
+    }
+
+    #addPurposeToText(purpose){
+        if (purpose.invertible == "true" && this.inverted){ //purpose.invertible is being stored as a string
+            if (this.potency <= -2){
+                this.#addDescriptionText(purpose.purposeDescriptions["invHigh"], purpose.type, purpose.primaryType);
+            } else if (this.potency <= 1){
+                this.#addDescriptionText(purpose.purposeDescriptions["invMid"], purpose.type, purpose.primaryType);
+            } else{
+                this.#addDescriptionText(purpose.purposeDescriptions["invLow"], purpose.type, purpose.primaryType);
+            }
+        } else{
+            if (this.potency >= 2){
+                this.#addDescriptionText(purpose.purposeDescriptions["high"], purpose.type, purpose.primaryType);
+            } else if (this.potency >= -1){
+                this.#addDescriptionText(purpose.purposeDescriptions["mid"], purpose.type, purpose.primaryType);
+            } else{
+                this.#addDescriptionText(purpose.purposeDescriptions["low"], purpose.type, purpose.primaryType);
+            }
+        }
+    }
+    
+    #addDescriptionText(text, componentType, specifier){ //specifier is optional controlling the underlines, -1 when not in use
+        const textElement = document.createElement("span");
+        if (componentType){
+            textElement.style.color = this.#getDescriptionColor(componentType);
+        }
+        if (specifier){
+            const underlineColor = this.#getDescriptionUnderlineColor(specifier);
+            textElement.style.textDecoration = "underline " + underlineColor;
+        }
+        textElement.innerHTML = text;
+        //textElement.className = "";
+
+        this.textBox.appendChild(textElement);
+    }
+
+    #getDescriptionColor(type){
+        switch(type){
+            case "Void":
+                return "#8A2BE2";
+            case "Form":
+                return "#3CB371";
+            case "Path":
+                return "#DAA520";
+            case "Purpose":
+                return "#A0AECD";
+            case "Enhancement":
+                return "#6495ED";
+            case "Refund":
+                return "#BC8F8F";
+            case "Trigger":
+                return "#FA8072";
+            default:
+                return "#000000";
+        }
+    }
+
+    #getDescriptionUnderlineColor(vis){
+        switch(vis){
+            case "Design":
+                return "#DBA463";
+            case "Nature":
+                return "#3CB371";
+            case "Eath":
+                return "#5A4E44";
+            case "Flesh":
+                return "#E86A73";
+            case "Heat":
+                return "#B4202A";
+            case "Flow":
+                return "#588DBE";
+            default:
+                logText("Underline color for wand description failed, defaulting to black.");
+                return "#000000";
+        }
+    }
+
+    #errorTest() {
+        this.#clearErrors();
+        let fatalErrors = false;
+
+        if (!this.pathComponent){
+            this.#addError(true, "A spell block requires a Path component!");
+            fatalErrors = true;
+        }
+        if (!this.formComponent){
+            this.#addError(true, "A spell block requires a Form component!");
+            fatalErrors = true;
+        }
+        if (this.purposeComponents.length == 0){
+            this.#addError(true, "A spell block requires at least one Purpose component!");
+            fatalErrors = true;
+        }
+        if (!this.#areAllPurposesInvertible() && this.voidComponent && this.purposeComponents.length != 0){
+            this.#addError(false, "A spell block includes a \"Nothing\", but one or more of its Purposes are not invertible and unaffected.");
+        }
+        if (this.spells.length > 7) {
+            this.#addError(false, "One or more spell blocks are quite long. This may result in goofy displays or extremely high mana costs.");
+        }
+        if (this.#findAllComponentsByType("Trigger").length > 1){
+            this.#addError(true, "A spell block may only contain up to one Trigger!");
+            fatalErrors = true;
+        }
+        if (this.#findAllComponentsByType("Path").length > 1){
+            this.#addError(true, "A spell block may only contain up to one Path!");
+            fatalErrors = true;
+        }
+        if (this.#findAllComponentsByType("Form").length > 1){
+            this.#addError(true, "A spell block may only contain up to one Form!");
+            fatalErrors = true;
+        }
+
+        return fatalErrors;
+    }
+
+    #clearErrors(){
+        clearChildren(this.errorBox);
+    }
+
+    #addError(fatal, text){
+        const error = document.createElement("div");
+        const icon = document.createElement("img");
+        const errorMsg = document.createElement("div");
+        error.className = "modalFormError";
+        icon.className = "errorIcon";
+        errorMsg.className = "modalFormErrorMessage";
+        if (fatal){
+            icon.src = "images/ui/red-error.png";
+        } else{
+            icon.src = "images/ui/yellow-error.png";
+        }
+        errorMsg.innerHTML = text;
+        error.appendChild(icon);
+        error.appendChild(errorMsg);
+        this.errorBox.appendChild(error);
+    }
+
+    #findComponentByType(type) {
+        for (let component of this.spells) {
+            if (component.type == type) {
+                return component;
+            }
+        }
+    }
+
+    #findAllComponentsByType(type) {
+        const result = [];
+        for (let component of this.spells) {
+            if (component.type == type) {
+                result.push(component);
+            }
+        }
+        return result;
+    }
+
+    #areAllPurposesInvertible() {
+        let result = true;
+        for (let purpose of this.purposeComponents){
+            if (purpose.invertible == "false"){ //typeless (but not *actually* typeless) my beloathed
+                result = false;
+            }
+        }
+        return result;
+    }
+}
--- src/wand.js
+++ src/wand.js
@@ -5,6 +5,7 @@ import { findComponentByName, componentList } from "../main.js";
 import { handleDeleteWandPress } from "./addNewWand.js";
 import { clearChildren } from "./elementHelpers.js";
 import { saveJSONFile } from "./json.js";
+import { spellBlock } from "./spellBlock.js";
 
 export class wand {
     constructor(name, flavor, image, slotsByName) {
@@ -230,224 +231,16 @@ export class wand {
 
     #compileSpell() {
         clearChildren(this.wordyDescriptionElement); //clear old children
-        const spellBlocks = this.#detectSpellBlocks();
-        for (let spellBlock of spellBlocks) {
-            if (this.#errorTestSpellBlock(spellBlock)) {
-                this.#addDescriptionText("Spell has one or more fatal errors and cannot be compiled.");
-            } else { //not terminating errors, just displaying user errors
-
-                let inverted = false;
-                const pathComponent = this.#findComponentByType(spellBlock, "Path");
-                const formComponent = this.#findComponentByType(spellBlock, "Form");
-                const enhancementComponents = this.#findAllComponentsByType(spellBlock, "Enhancement");
-                const purposeComponents = this.#findAllComponentsByType(spellBlock, "Purpose");
-                const triggerComponent = this.#findComponentByType(spellBlock, "Trigger");
-
-                if (this.#findComponentByType(spellBlock, "Void")) {
-                    inverted = true;
-                }
-
-                let potency = 0;
-                for (let component of spellBlock) {
-                    potency += component.potencyModifier;
-                }
-
-                this.#addDescriptionText(pathComponent.pathDescription, pathComponent.type);
-                this.#addDescriptionText(formComponent.formDescription, formComponent.type);
-                if (enhancementComponents.length > 0) {
-                    for (let enhancement of enhancementComponents) {
-                        this.#addDescriptionText(enhancement.enhancementDescription, enhancement.type);
-                    }
-                }
-                this.#fillPurposeText(purposeComponents, inverted, potency);
-                if (triggerComponent) {
-                    this.#addDescriptionText(triggerComponent.triggerDescription, triggerComponent.type);
-                }
-            }
+        const nonCompiledSpellBlocks = this.#detectSpellBlocks();
+        for (let spellCollection of nonCompiledSpellBlocks) {
+            const block = new spellBlock(spellCollection, 0, this.spellDescriptionElement);
+            block.compileSpell();
         }
         this.saveToFile();
     }
 
     #detectSpellBlocks() { //TODO separates the spell into multiple blocks, returns an array of arrays with the split component at the front. may require a new component
         return [this.slotsByObject];
-    }
-
-    #errorTestSpellBlock(spellBlock) {
-        this.#clearErrors();
-        const voidComponent = this.#findComponentByType(spellBlock, "Void");
-        const pathComponent = this.#findComponentByType(spellBlock, "Path");
-        const formComponent = this.#findComponentByType(spellBlock, "Form");
-        const purposeComponents = this.#findAllComponentsByType(spellBlock, "Purpose");
-
-        let fatalErrors = false;
-
-        if (!pathComponent){
-            this.#addError(true, "A spell block requires a Path component!");
-            fatalErrors = true;
-        }
-        if (!formComponent){
-            this.#addError(true, "A spell block requires a Form component!");
-            fatalErrors = true;
-        }
-        if (purposeComponents.length == 0){
-            this.#addError(true, "A spell block requires at least one Purpose component!");
-            fatalErrors = true;
-        }
-        if (!this.#allPurposeComponentsAreInvertible(purposeComponents) && voidComponent && purposeComponents.length != 0){
-            this.#addError(false, "A spell block includes a \"Nothing\", but one or more of its Purposes are not invertible and unaffected.");
-        }
-        if (spellBlock.length > 7) {
-            this.#addError(false, "One or more spell blocks are quite long. This may result in goofy displays or extremely high mana costs.");
-        }
-        if (this.#findAllComponentsByType(spellBlock, "Trigger").length > 1){
-            this.#addError(true, "A spell block may only contain up to one Trigger!");
-            fatalErrors = true;
-        }
-        if (this.#findAllComponentsByType(spellBlock, "Path").length > 1){
-            this.#addError(true, "A spell block may only contain up to one Path!");
-            fatalErrors = true;
-        }
-        if (this.#findAllComponentsByType(spellBlock, "Form").length > 1){
-            this.#addError(true, "A spell block may only contain up to one Form!");
-            fatalErrors = true;
-        }
-
-        return fatalErrors;
-    }
-
-    #clearErrors(){
-        clearChildren(this.errorBoxDescriptionElement);
-    }
-
-    #addError(fatal, text){
-        const error = document.createElement("div");
-        const icon = document.createElement("img");
-        const errorMsg = document.createElement("div");
-        error.className = "modalFormError";
-        icon.className = "errorIcon";
-        errorMsg.className = "modalFormErrorMessage";
-        if (fatal){
-            icon.src = "images/ui/red-error.png";
-        } else{
-            icon.src = "images/ui/yellow-error.png";
-        }
-        errorMsg.innerHTML = text;
-        error.appendChild(icon);
-        error.appendChild(errorMsg);
-        this.errorBoxDescriptionElement.appendChild(error);
-    }
-
-    #findComponentByType(spellBlock, type) {
-        for (let component of spellBlock) {
-            if (component.type == type) {
-                return component;
-            }
-        }
-    }
-
-    #findAllComponentsByType(spellBlock, type) {
-        const result = [];
-        for (let component of spellBlock) {
-            if (component.type == type) {
-                result.push(component);
-            }
-        }
-        return result;
-    }
-
-    #allPurposeComponentsAreInvertible(purposeComponents) {
-        let result = true;
-        for (let purpose of purposeComponents){
-            if (purpose.invertible == "false"){ //typeless (but not *actually* typeless) my beloathed
-                result = false;
-            }
-        }
-        return result;
-    }
-
-    #fillPurposeText(purposeComponents, inverted, potency) {
-        for (let i = 0; i < purposeComponents.length - 1; i++){
-            this.#addPurposeToText(purposeComponents[i], inverted, potency);
-            this.#addDescriptionText(" and ");
-        }
-        this.#addPurposeToText(purposeComponents[purposeComponents.length - 1], inverted, potency);
-        this.#addDescriptionText(". ");
-    }
-
-    #addPurposeToText(purpose, inverted, potency){ //beautifying the text using colored spans might be a good idea. maybe change it from innerHTML?
-        if (purpose.invertible == "true" && inverted){ //purpose.invertible is being stored as a string
-            if (potency <= -2){
-                this.#addDescriptionText(purpose.purposeDescriptions["invHigh"], purpose.type, purpose.primaryType);
-            } else if (potency <= 1){
-                this.#addDescriptionText(purpose.purposeDescriptions["invMid"], purpose.type, purpose.primaryType);
-            } else{
-                this.#addDescriptionText(purpose.purposeDescriptions["invLow"], purpose.type, purpose.primaryType);
-            }
-        } else{
-            if (potency >= 2){
-                this.#addDescriptionText(purpose.purposeDescriptions["high"], purpose.type, purpose.primaryType);
-            } else if (potency >= -1){
-                this.#addDescriptionText(purpose.purposeDescriptions["mid"], purpose.type, purpose.primaryType);
-            } else{
-                this.#addDescriptionText(purpose.purposeDescriptions["low"], purpose.type, purpose.primaryType);
-            }
-        }
-    }
-
-    #addDescriptionText(text, componentType, specifier){ //specifier is optional, -1 when not in use
-        const textElement = document.createElement("span");
-        if (componentType){
-            textElement.style.color = this.#getDescriptionColor(componentType);
-        }
-        if (specifier){
-            const underlineColor = this.#getDescriptionUnderlineColor(specifier);
-            textElement.style.textDecoration = "underline " + underlineColor;
-        }
-        textElement.innerHTML = text;
-        //textElement.className = "";
-
-        this.wordyDescriptionElement.appendChild(textElement);
-    }
-
-    #getDescriptionColor(type){
-        switch(type){
-            case "Void":
-                return "#8A2BE2";
-            case "Form":
-                return "#3CB371";
-            case "Path":
-                return "#DAA520";
-            case "Purpose":
-                return "#A0AECD";
-            case "Enhancement":
-                return "#6495ED";
-            case "Refund":
-                return "#BC8F8F";
-            case "Trigger":
-                return "#FA8072";
-            default:
-                return "#000000";
-        }
-    }
-
-    #getDescriptionUnderlineColor(vis){
-        switch(vis){
-            case "Design":
-                return "#DBA463";
-            case "Nature":
-                return "#3CB371";
-            case "Eath":
-                return "#5A4E44";
-            case "Flesh":
-                return "#E86A73";
-            case "Heat":
-                return "#B4202A";
-            case "Flow":
-                return "#588DBE";
-            default:
-                logText("Underline color for wand description failed, defaulting to black.");
-                return "#000000";
-        }
     }
 
     saveToFile() {
